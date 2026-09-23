54eb5374fb10d4c86e273d29c09400f26497aa86
Strikee1225 <6811310013@tsu.ac.th>
2026-09-23 22:28:13 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,18 @@
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
+
+print("MONGO_URI EXISTS:", bool(MONGO_URI))
+
+if MONGO_URI:
+    parsed = urlparse(MONGO_URI)
+    print("MONGO USER:", parsed.username)
+    print("MONGO HOST:", parsed.hostname)
 
 client = MongoClient(MONGO_URI)
 
@@ -14,4 +22,8 @@ events_collection = db["events"]
 registrations_collection = db["registrations"]
 messages_collection = db["messages"]
 
-print("Connected to MongoDB successfully!")
+try:
+    client.admin.command("ping")
+    print("MONGODB PING: SUCCESS")
+except Exception as e:
+    print("MONGODB PING ERROR:", e)
